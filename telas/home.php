3baa78dcde295dc992864cc57e2c5f198e458216
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 15:30:00 -0300

    Adicionando boas vindas e imagem na home

--- telas/home.php
+++ telas/home.php
@@ -1,6 +1,2 @@
-<h1>Página Inicial</h1>
-<hr>
-
-<div>
-  Bem-vind@ a página Inicial!
-</div>
+<h1 style="color: #FFF;">Bem-vind@ à lista de contatos em PHP!</h1>
+<img src="/img/hello.svg" alt="" width="50%" class="mt-5">
