dac255963648015fc5384a785b8905c59f45435a
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 15:28:54 -0300

    Adicionando bootstrap na navbar

--- telas/menu.php
+++ telas/menu.php
@@ -1,7 +1,31 @@
-<a class="btn btn-outline-dark" href="/">Inicio</a>
-<a class="btn btn-outline-dark" href="/login">Login</a>
-<a class="btn btn-outline-dark" href="/cadastro">Cadastro</a>
-<a class="btn btn-outline-dark" href="/admin">Admin</a>
-<a class="btn btn-outline-dark" href="/listar">Listar</a>
-<a class="btn btn-outline-dark" href="/relatorio">Relatórios</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CRUD</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/login">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/cadastro">Cadastro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/admin">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/listar">Listar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/relatorio">Relatórios</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <hr>
