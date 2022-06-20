5d3662bea6823d225a219e662cbd9c1a6989b44d
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-20 12:43:35 -0300

    Adicionando rota ao botão via JavaScript

--- telas/home.php
+++ telas/home.php
@@ -1,4 +1,11 @@
-<h1 style="color: #212529;">Bem-vind@, à lista de contatos em PHP!</h1>
+<h1 style="color: #212529"><b>Bem-vind</b>@, à lista de contatos em PHP!</h1>
 <div class="container text-center">
-<img src="/img/hello.svg" alt="" width="60%" class="mt-5">
+  <img src="/img/hello.svg" alt="" width="60%" class="mt-5">
+  <button id="first-button" href="/login">→</button>
 </div>
+
+<script>
+document.getElementById('first-button').addEventListener('click', function () {
+    window.location.href = '/login';
+});
+</script>
