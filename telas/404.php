c2c98d4d84146ac1f9c415c485a15a829fb569b3
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-19 23:52:57 -0300

    Centralizando componentes na 404

--- telas/404.php
+++ telas/404.php
@@ -1,2 +1,4 @@
-<h1>Página não Encontrada :(</h1>
-<img src="img/404.svg" width="45%" alt="">
+<div class="container text-center">
+  <img src="img/404.svg" width="50%" alt="">
+  <h1>Página não Encontrada :(</h1>
+</div>
