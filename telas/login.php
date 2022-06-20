a7f20b9c9a52b50b9ec6cf1c528923b7f1ac81aa
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-19 23:51:34 -0300

    Alinhando componentes ao centro

--- telas/login.php
+++ telas/login.php
@@ -1,7 +1,18 @@
-<h1>Login</h1>
+<h1 class="text-center">Login</h1>
 <hr>
-<form action="">
-  <input class="form-control mb-3" type="text" placeholder="email">
-  <input class="form-control mb-3" type="password" placeholder="senha">
-  <button class="btn btn-primary">Enviar</button>
-</form>
+<div class="row">
+  <div class="col-8 align-self-center jus">
+    <img src="./img/login.svg" alt="Login" width="80%">
+  </div>
+  <div class="col align-self-center jus">
+    <form method="post">
+      <input class="form-control mb-3" type="text" placeholder="email">
+      <input class="form-control mb-3" type="password" placeholder="senha">
+      <div class="checkbox mb-3">
+      <label for="">
+    <input type="checkbox"> Lembrar de mim
+  </label>
+  </div>
+      <button id="botaum" class="btn btn-primary">Enviar</button>
+    </form>
+</div>
