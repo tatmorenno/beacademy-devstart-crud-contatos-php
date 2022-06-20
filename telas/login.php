d16d5733a4469063afd6b38dae942bec5bccb59a
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-20 12:44:48 -0300

    Adicionando 'Deseja criar uma conta?'

--- telas/login.php
+++ telas/login.php
@@ -2,17 +2,18 @@
 <hr>
 <div class="row">
   <div class="col-8 align-self-center jus">
+    <a id="anchor" class="align-self-left" href="/cadastro"><i class="bi bi-person-plus"></i> Deseja criar uma conta?</a>
     <img src="./img/login.svg" alt="Login" width="80%">
   </div>
   <div class="col align-self-center jus">
     <form method="post">
-      <input class="form-control mb-3" type="text" placeholder="email">
-      <input class="form-control mb-3" type="password" placeholder="senha">
+      <input class="form-control mb-3" type="email" placeholder="email" required>
+      <input class="form-control mb-3" type="password" placeholder="senha" required>
       <div class="checkbox mb-3">
       <label for="">
     <input type="checkbox"> Lembrar de mim
   </label>
   </div>
-      <button id="botaum" class="btn btn-primary">Enviar</button>
+      <button id="botaum" class="btn btn-primary"><i class="bi bi-send"></i> Enviar</button>
     </form>
 </div>
