b66d5c919ee01d83a38719631f7e6824313b1206
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-19 23:51:49 -0300

    Alinhando ao centro

--- telas/listar.php
+++ telas/listar.php
@@ -1,12 +1,19 @@
-<h1>Listar Contatos</h1>
-
+<div class="row">
+  <div class="col-1 align-self-center jus">
+    <img src="../img/listar.svg" width="90%" alt="">
+  </div>
+  <div class="col align-self-center jus">
+    <h1>Listar Contatos</h1>
+  </div>
+</div>
+<hr>
 <table class="table table-hover table-striped">
   <thead class="table-dark">
     <tr>
       <th>Nome</th>
       <th>Email</th>
       <th>Telefone</th>
-      <th>Ações</th>
+      <th class="text-center">Ações</th>
     </tr>
     <tbody>
       <?php
@@ -17,9 +24,16 @@
             echo '<td>' . $partes[1] . '</td>';
             echo '<td>' . $partes[2] . '</td>';
             echo "<td>
-                  <a href='/excluir?id={$posicao}' class='btn btn-danger btn-sm'>Excluir</a>
-                  <a href='/editar?id={$posicao}' class='btn btn-warning btn-sm'>Editar</a>
-                  </td>";
+            <div class='container text-center'>
+
+                  <a href='/excluir?id={$posicao}' class='btn btn-danger btn-sm'>
+                    <i class='bi bi-trash3-fill'><span> Excluir</span></i>
+                  </a>
+                  <a href='/editar?id={$posicao}' class='btn btn-warning btn-sm'>
+                    <i class='bi bi-pencil-square'><span> Editar</span></i>
+                  </a>
+                  </td>
+                  </div>";
           echo '<tr>';
         }
       ?>
